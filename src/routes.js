75818be0af378c7e6baefa3b0fc8711b1873ae91
Mateus Granero <mateusgranero@gmail.com>
2020-01-28 21:18:42 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Wold!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Mateus Granero',
+    email: 'mateusgranero@gmail.com',
+    password_hash: '123465987',
+  });
+  return res.json(user);
+});
 
 export default routes;
